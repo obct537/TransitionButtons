9d863e7112cc1bd86af0a8df2f5dac7dd68275fa
Sam Schwartz <obct537@gmail.com>
2013-11-15 16:17:42 -0600

    Removed useless Extensions

--- src/app/statebuttons/Extensions/Install.py.py
+++
@@ -1,8 +0,0 @@
-
-from Products.CMFCore.utils import getToolByName
-
-def uninstall(portal):
-    profile = 'profile-app.statebuttons:uninstall'
-    setup_tool = getToolByName(portal, 'portal_setup')
-    setup_tool.runAllImportStepsFromProfile(profile)
-    return "Ran all uninstall steps."
