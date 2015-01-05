ec3f7c9556dfe406e128bb563be0a72e2dc0699e
Sam Schwartz <obct537@gmail.com>
2015-01-05 16:20:24 -0600

    Fixed improper cookie path

--- src/uwosh/transitionbuttons/browser/resources/buttons.js
+++ src/uwosh/transitionbuttons/browser/resources/buttons.js
@@ -91,7 +91,7 @@
             var d = new Date();
             d.setTime(d.getTime() + (24*60*60*1000));
             var expires = "expires="+d.toUTCString();
-            document.cookie = "buttonsHidden=true; " + expires;
+            document.cookie = "buttonsHidden=true; Path=/;" + expires;
         }
 
         function removeCookie() {
@@ -247,6 +247,7 @@
                 showButtons();
             }
             else {
+                removeCookie();
                 hideButtons();
                 setCookie();  
             }
